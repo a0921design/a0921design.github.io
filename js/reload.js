46d3eae0dd2ae8dcbda02ee2252d0849ce9fe632
aki0921 <66670058+aki0921@users.noreply.github.com>
2022-03-25 23:19:30 +0900

    Add files via upload

---
+++ js/reload.js
@@ -0,0 +1,23 @@
+const breakPoint = 599;
+  let resizeFlag;
+
+  window.addEventListener('load',()=>{
+    if( breakPoint > window.innerWidth){
+      resizeFlag = false;
+    }else{
+      resizeFlag = true;
+    }
+    resizeWindow();
+  },false);
+
+  const resizeWindow = () =>{
+    window.addEventListener('resize',()=>{
+      if( breakPoint > window.innerWidth && resizeFlag){
+        window.location.reload();
+        resizeFlag = false;
+      } 
+      else if ( breakPoint <= window.innerWidth && !(resizeFlag)) {
+        resizeFlag = true;
+      }
+    },false);
+  }

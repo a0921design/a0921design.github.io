9a94a1927e35a811a0989820fc15474fe96b6693
a0921design <66670058+a0921design@users.noreply.github.com>
2022-07-18 10:27:36 +0900

    Add files via upload

--- assets/js/tabletResize.js
+++ assets/js/tabletResize.js
@@ -5,7 +5,7 @@ onResize();
 function onResize(){
 
 	if(ua.isTablet){
-			$("#Viewport").attr("content","width=1200,maximum-scale=1,user-scalable=no,shrink-to-fit=no");
+			$("#Viewport").attr("content","width=1400,maximum-scale=1,user-scalable=no,shrink-to-fit=no");
 	}
 }
 
